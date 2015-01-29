4f17b6782aeb23b9731722d7502cf5eebcda518d
moziqi <709847739@qq.com>
2015-01-29 14:28:52 +0800

    仿material design的按钮

--- app/src/main/java/org/moziqi/generic/company/activity/MainActivity.java
+++ app/src/main/java/org/moziqi/generic/company/activity/MainActivity.java
@@ -1,9 +1,11 @@
-package org.moziqi.generic;
+package org.moziqi.generic.company.activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.moziqi.generic.R;
 
 
 public class MainActivity extends ActionBarActivity {
